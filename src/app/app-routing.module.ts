6dca0b07f229a038a6fb8a9d5842ee0cdf5bf8d9
berdmival <mtsby7049874@gmail.com>
2021-02-26 11:43:36 +0300

    the skeleton of the footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
+import {MainContentComponent} from "./main-content/main-content.component";
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: MainContentComponent},
   {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
